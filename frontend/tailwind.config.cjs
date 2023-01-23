01b998fdb83a08109644355a8bbc35313ca4ed2f
ryrob <ryanrobertson242@gmail.com>
2023-01-23 08:43:57 -0600

    Extended tailwind colors so default colors could be used also

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,16 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      'texas-orange': '#bf5700',
-      'texas-dark-gray': '#333f48',
-      'texas-light-gray': '#9cadb7',
-      'texas-white': '#ffffff',
-      'texas-black': '#000000',
-      'texas-dark-blue': '#005f86',
-      'texas-cream': '#d6d2c4',
+    extend: {
+      colors: {
+        'texas-orange': '#bf5700',
+        'texas-dark-gray': '#333f48',
+        'texas-light-gray': '#9cadb7',
+        'texas-white': '#ffffff',
+        'texas-black': '#000000',
+        'texas-dark-blue': '#005f86',
+        'texas-cream': '#d6d2c4',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
